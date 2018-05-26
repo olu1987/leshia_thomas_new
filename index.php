d683383876e9d409fe6cadf2b5038e025f3b9b41
Olu Meduoye <olu.meduoye@tvplayer.com>
2018-05-26 12:29:55 +0100

    Removing uneeded sections from home page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
         <!-- Add your site or application content here -->
         <div id="home" class="container">
-            <div class="row text-center header" data-parallax="scroll" data-image-src="img/olu-header.jpg" data-position-x="1000px">
+            <div class="row text-center header">
                 <div class="overlay">
 
                 </div>
@@ -21,72 +21,6 @@
                     </div>
                 </div>
             </div>
-            </div>
-
-    <div id="skills" class="container">
-        <div class="skills-wrap">
-        <div class="row skills text-center">
-            <div class="col-sm-6 text-uppercase text-left html">
-                 <h2>html and css</h2>
-                 <ul class="list">
-                     <li class="list-item">html5</li>
-                     <li class="list-item">css3</li>
-                     <li class="list-item">sass</li>
-                     <li class="list-item">css modular patterns (smascss,bemmcss)</li>
-                     <li class="list-item">bootstrap</li>
-                     <li class="list-item">responsive design</li>
-                 </ul>
-            </div>
-            <div class="col-sm-6 text-uppercase text-right javascript">
-                 <h2>javascript</h2>
-                 <ul class="list">
-                     <li class="list-item">vanilla javascript</li>
-                     <li class="list-item">es6</li>
-                     <li class="list-item">jquery</li>
-                     <li class="list-item">react js</li>
-                     <li class="list-item">angular js</li>
-                     <li class="list-item">node js (npm)</li>
-                 </ul>
-            </div>
-        </div>
-        <div class="row skills">
-            <div class="col-sm-6 lower text-uppercase text-left frontend-build">
-                 <h2>frontend build tools</h2>
-                 <ul class="list">
-                     <li class="list-item">gulp</li>
-                     <li class="list-item">grunt</li>
-                     <li class="list-item">laravel elixir</li>
-                     <li class="list-item">babel</li>
-                     <li class="list-item">browserify</li>
-                 </ul>
-            </div>
-            <div class="col-sm-6 lower text-uppercase text-right backend">
-                <h2>other skills</h2>
-                 <ul class="list">
-                     <li class="list-item">cross-browser solutions</li>
-                     <li class="list-item">php</li>
-                     <li class="list-item">zend</li>
-                     <li class="list-item">mysql</li>
-                     <li class="list-item">photoshop</li>
-                     <li class="list-item">git</li>
-                     <li class="list-item">agile methodologies</li>
-                     <li class="list-item">team leadership</li>
-                 </ul>
-            </div>
-        </div>
-        </div>
-        <div id="about" class="row text-center about">
-            <div class="col-sm-8 col-sm-offset-2">
-                <h1 class="text-uppercase">About Me</h1>
-                <p>I like to build things that work and look good. I'm constantly improving and learning and I believe that
-                    anyone can learn from anyone. I pride myself on finding innovative and modern solutions to problems and I love
-                    teamwork. Whether it be creating pixel perfect deliverables, building beautiful user interfaces
-                    using modern javascript frameworks like react and angular, or communicating with tact and clarity with all
-                    stakeholders, all are done in the same spirit of excellence that I apply
-                    to all my work. If you're interested in acquiring my expertise, do get in
-                <a href="#" class="fade-out-link gold-color" data-link="contact.php">touch</a>.</p>
-            </div>
-        </div>
             <div id="portfolio" class="row portfolio">
                 <div class="col-md-12">
                     <a data-link="simplestream-react-tvguide.php" class="tile col-md-4 col-sm-6 ss fade-out-link">
