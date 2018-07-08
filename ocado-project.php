a8b6f72ec8c6501fedc07a9ee03fc69cc757f716
Olu Meduoye <olu.meduoye@tvplayer.com>
2018-07-08 22:19:09 +0100

    starting page for ocado project

---
+++ ocado-project.php
@@ -0,0 +1,38 @@
+<?php $title = 'Digital Designer from London';
+      $body = 'home';
+      $spinnerLogo = 'img/lt-new-icon.png';
+?>
+<?php include 'header.php'; ?>
+<div class="container">
+   <div class="row">
+     <div class="col-md-10 col-md-offset-1">
+     <img class="img-responsive" src="img/ocado-valentine-header.jpg" />  
+     </div>      
+   </div>   
+   <div class="row">
+     <div class="col-md-10 col-md-offset-1">
+       <h1 class="text-capitalize">moodboard one</h1>    
+     <img class="img-responsive" src="img/ocado-moodboard-one.jpg" />  
+     </div>      
+   </div>   
+   <div class="row ocado-tiles">
+     <div class="col-md-5 col-md-offset-1">
+       <img class="img-responsive" src="img/ocado-tile-pink-one.png" />  
+     </div>      
+     <div class="col-md-5">
+       <img class="img-responsive" src="img/ocado-tile-grey.png" />  
+     </div>
+   </div>
+   <div class="row ocado-tiles">         
+     <div class="col-md-5 col-md-offset-1">
+       <img class="img-responsive" src="img/ocado-tile-pink-two.png" />  
+     </div>      
+     <div class="col-md-5">
+       <img class="img-responsive" src="img/ocado-tile-red.png" />  
+     </div>
+     <div class="col-md-10">
+       <img class="img-responsive" src="img/valentine-bird.png" />     
+     </div>      
+   </div>   
+</div>
+<?php include 'footer.php'; ?>
