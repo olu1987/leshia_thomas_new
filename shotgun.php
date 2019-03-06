a2ad327771441ad9ede0127a52aa3cd9aa02b256
Olu Meduoye <olu@claymoreminds.com>
2019-03-06 16:10:52 +0000

    shotgun reordering rows

--- shotgun.php
+++ shotgun.php
@@ -20,6 +20,46 @@
       </div>
       <div class="row feature-row">
         <div class="col-md-4 col-md-offset-1 col-sm-6">
+           <h2>Onboarding</h2>
+           <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
+        <div class="col-md-6 col-sm-6 col-md-offset-1 px-0">
+          <img class="img-responsive" src="img/shotgun-app-welcome.png" />
+        </div>
+      </div> 
+      <div class="row feature-row reverse">
+        <div class="col-md-7 col-sm-6 col-md-offset-1">
+          <img class="img-responsive" src="img/shotgun-wireframe.png" />
+        </div>
+        <div class="col-md-4 col-sm-6">
+           <h2>wireframes</h2>
+           <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
+      </div>
+      <div class="row feature-row">
+        <div class="col-md-4 col-md-offset-1 col-sm-6">
+           <h2>Account Page</h2>
+           <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+            <img class="img-responsive" src="img/shotgun-account-buttons.png" />
+        </div>
+        <div class="col-md-6 col-sm-6 col-md-offset-1 px-0">
+          <img class="img-responsive" src="img/shotgun-account-page.png" />
+        </div>
+      </div>
+      <div class="row feature-row">
+        <div class="col-md-4 col-md-offset-1 col-sm-6">
            <h2>Colors</h2>
            <p>Shotgun are a startup that market themselves as an "uber for builders".
             The idea being that people post jobs on the app, and builders are able to pick 
@@ -42,46 +82,6 @@
             the job based on their proximity to the customer. I'm continuing to work with Shotgun
             as they prepare to put the app into alpha testing in the very near future.
             </p>
-        </div>
-      </div>
-      <div class="row feature-row reverse">
-        <div class="col-md-7 col-sm-6 col-md-offset-1">
-          <img class="img-responsive" src="img/shotgun-wireframe.png" />
-        </div>
-        <div class="col-md-4 col-sm-6">
-           <h2>wireframes</h2>
-           <p>Shotgun are a startup that market themselves as an "uber for builders".
-            The idea being that people post jobs on the app, and builders are able to pick 
-            the job based on their proximity to the customer. I'm continuing to work with Shotgun
-            as they prepare to put the app into alpha testing in the very near future.
-            </p>
-        </div>
-      </div>
-      <div class="row feature-row">
-        <div class="col-md-4 col-md-offset-1 col-sm-6">
-           <h2>Onboarding</h2>
-           <p>Shotgun are a startup that market themselves as an "uber for builders".
-            The idea being that people post jobs on the app, and builders are able to pick 
-            the job based on their proximity to the customer. I'm continuing to work with Shotgun
-            as they prepare to put the app into alpha testing in the very near future.
-            </p>
-        </div>
-        <div class="col-md-6 col-sm-6 col-md-offset-1 px-0">
-          <img class="img-responsive" src="img/shotgun-app-welcome.png" />
-        </div>
-      </div> 
-      <div class="row feature-row">
-        <div class="col-md-4 col-md-offset-1 col-sm-6">
-           <h2>Account Page</h2>
-           <p>Shotgun are a startup that market themselves as an "uber for builders".
-            The idea being that people post jobs on the app, and builders are able to pick 
-            the job based on their proximity to the customer. I'm continuing to work with Shotgun
-            as they prepare to put the app into alpha testing in the very near future.
-            </p>
-            <img class="img-responsive" src="img/shotgun-account-buttons.png" />
-        </div>
-        <div class="col-md-6 col-sm-6 col-md-offset-1 px-0">
-          <img class="img-responsive" src="img/shotgun-account-page.png" />
         </div>
       </div>
       <div class="row footer-text-row">
