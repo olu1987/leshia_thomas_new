1705731fb1fed817ce59bd5d495bb6a7a72ba751
Olu Meduoye <olu@claymoreminds.com>
2019-03-06 18:41:04 +0000

    switching out ocado logo

--- tiles/ocado.php
+++ tiles/ocado.php
@@ -1,5 +1,5 @@
 <a data-link="ocado-project.php" class="tile col-md-4 col-sm-6 ocado fade-out-link">
-    <img class="img-responsive" src="img/ocado-tile.png"/>
+    <img class="img-responsive" src="img/ocado-logo-xs.png"/>
     <div class="title-wrap">
         <h3>
         <?php include 'partials/previous-next.php' ?>
