4837ff7760478cbef6c19847e29ad3373a186b22
Olu Meduoye <olu@claymoreminds.com>
2019-03-06 16:41:22 +0000

    redoing bodhan page

--- bodhan.php
+++ bodhan.php
@@ -1,49 +1,64 @@
-<?php $title = 'Bodhan Consultants Logo';
-      $spinnerLogo = 'img/bodhan-tile.png';
+<?php $title = 'Anthony Devonish | Logo Design';
+      $spinnerLogo = 'img/ad-logo.png';
       $body = 'not-home bodhan-bg'
 ?>
 <?php include 'header.php'; ?>
 
     <!-- Add your site or application content here -->
-    <div class="container bodhan-bg">
-        <div class="row pt-20">
-            <div class="col-md-6 col-md-offset-3 text-center">
-                <img class="img-responsive page-header-logo" src="img/bodhan-tile.png"/>
-            </div>
+    <div class="container">
+      <div class="row header-row">
+        <div class="col-md-4 col-md-offset-1">
+          <img class="img-responsive" src="img/bodhan-logo-xs.png" />
         </div>
-        <div class="row mb-50 text-center">
-            <div class="col-md-6 col-md-offset-3 intro-col">
-                <h2>Bodhan Consultants<br><small> Logo</small></h2> 
-                <p>Bodhan Consultants are in their own words - "
-                a communications consultancy that helps financial companies achieve their communications goals. ".
-                Before the launch of their website, I was asked to create a logo that embodied the
-                the principles that they believe are at the core of their company. We were both very 
-            pleased with the results and I have continued to work with them on a <br>consulting basis.</p>
-            </div>
+        <div class="col-md-6">
+          <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
         </div>
-        <div class="row ">
-            <div class="col-md-10 col-md-offset-1">
-                <img class="img-responsive" src="img/bodhan-devices-1.jpg"/>
-            </div>
+      </div>
+      <div class="row feature-row">
+        <div class="col-md-10 col-md-offset-1">
+          <img class="img-responsive" src="img/bodhan-typography-and-cards.png" />
         </div>
-        <div class="row task-description">
-            <div class="col-md-10 col-md-offset-1">
-                <img class="img-responsive" src="img/bodhan-cards.jpg"/>
-            </div>
+      </div>
+      <div class="row feature-row">
+        <div class="col-md-10 col-md-offset-1">
+           <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
         </div>
+      </div>
+      <div class="row feature-row">
+        <div class="col-md-8 col-md-offset-2">
+          <img class="img-responsive" src="img/bodhan-logo-xl.png" />
+        </div>
+      </div>
+      <div class="row footer-text-row pt-0">
+        <div class="col-md-8 col-md-offset-2">
+           <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
+      </div>
     </div>
-    <div class="container bodhan-bg">
-        <div class="row portfolio">
+    <div class="container">
+       <div class="row portfolio">
             <div class="col-md-12">
             <a data-anchor="home" data-link="index.php" class="tile home-tile col-md-4 col-sm-6 fade-out-link">
-            <h3> Home</h3>
-         </a>
-         <span class="previous">
-            <?php include 'tiles/bershka.php'; ?>
-        </span>
-        <span class="next">
-            <?php include 'tiles/lnt.php'; ?>
-        </span>
+              <h3> Home </h3>
+            </a>
+            <span class="previous">
+            <?php include 'tiles/darling.php'; ?>
+            </span>
+            <span class="next">
+            <?php include 'tiles/pull-and-bear.php'; ?>
+            </span>
             </div>
         </div>
     </div>
