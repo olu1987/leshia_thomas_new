c6224dcfddd89f1fed040107bda003636e4dd625
Olu Meduoye <olu@claymoreminds.com>
2019-03-08 18:19:04 +0000

    placement for ocado page text items

--- ocado-project.php
+++ ocado-project.php
@@ -19,11 +19,27 @@
         </div>
       </div>
       <div class="row feature-row">
-        <div class="col-md-10 col-md-offset-1">
+      <div class="col-md-6 ocado-overlapping-text-col one">
+         <h2>Mood Board</h2>
+          <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
+        <div class="col-md-12">
           <img class="img-responsive" src="img/ocado-new-mood-board.jpg" />
         </div>
       </div>
       <div class="row feature-row">
+        <div class="col-md-6 ocado-overlapping-text-col two">
+         <h2>typography and color</h2>
+          <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
         <div class="col-md-10 col-md-offset-1">
           <img class="img-responsive" src="img/ocado-typography.png" />
         </div>
@@ -34,11 +50,11 @@
         </div>
       </div>
       <div class="row feature-row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-4 col-md-offset-4 text-center">
         <h2>Logo Ideation</h2>  
         <p>Shotgun are a startup that market themselves as an "uber for builders".
             The idea being that people post jobs on the app, and builders are able to pick 
-            the job based on their proximity to the customer.
+            the job based on their proximity to the.
             </p>
         </div>
       </div>
@@ -48,22 +64,38 @@
         </div>
       </div>
       <div class="row feature-row">
+       <div class="col-md-6 ocado-overlapping-text-col one">
+         <h2>Shop in shop layouts</h2>
+          <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
         <div class="col-md-10 col-md-offset-1">
           <img class="img-responsive" src="img/ocado-ui-one.jpg" />
         </div>
       </div>
       <div class="row feature-row">
+      <div class="col-md-6 ocado-overlapping-text-col three">
+         <h2>Banners</h2>
+          <p>Shotgun are a startup that market themselves as an "uber for builders".
+            The idea being that people post jobs on the app, and builders are able to pick 
+            the job based on their proximity to the customer. I'm continuing to work with Shotgun
+            as they prepare to put the app into alpha testing in the very near future.
+            </p>
+        </div>
         <div class="col-md-10 col-md-offset-1">
           <img class="img-responsive" src="img/ocado-ui-two.jpg" />
         </div>
       </div>
       <div class="row feature-row">
-        <div class="col-md-7 col-sm-6 col-md-offset-1 px-0">
+        <div class="col-md-7 col-md-offset-1 px-0">
           <img class="img-responsive" src="img/ocado-color-options.jpg" />
         </div>
-        <div class="col-md-3 col-sm-6 col-md-offset-1">
+        <div class="col-md-3 ml-50">
            <img class="img-responsive" src="img/ocado-birds.png" />
-           <h2>Account Page</h2>
+           <h2>Asset Placement Mockups</h2>
            <p>Shotgun are a startup that market themselves as an "uber for builders".
             The idea being that people post jobs on the app, and builders are able to pick 
             the job based on their proximity to the customer. I'm continuing to work with Shotgun
