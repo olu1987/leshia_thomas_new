f8c36d8e09533cf6b18aa843a1d2d761408ab7ff
Olu Meduoye <olu@claymoreminds.com>
2019-03-08 18:45:12 +0000

    cache bust for css

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="build/css/application-1.0.css">
+    <link rel="stylesheet" href="build/css/application-1.1.css">
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans:300i,400|Cinzel:400,700|Arvo|Signika" rel="stylesheet">
 </head>
