72417a44fa3ca0b92fa9b31245cb205a80456f97
Olu Meduoye <olu@claymoreminds.com>
2019-03-09 16:37:35 +0000

    image for bodhan tile

--- tiles/bodhan.php
+++ tiles/bodhan.php
@@ -1,5 +1,5 @@
 <a data-link="bodhan.php" class="tile col-md-4 col-sm-6 bodhan fade-out-link">
-    <img class="img-responsive" src="img/bodhan-tile.png"/>
+    <img class="img-responsive" src="img/bodhan-tile-new.png"/>
     <div class="title-wrap">
         <h3>
         <?php include 'partials/previous-next.php' ?>
